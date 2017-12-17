6850090f8553bd653ed494a82e8d6096a11246ef
Blanka Orosz <oroszblanki@gmail.com>
2017-12-17 15:29:47 +0100

    login test

--- frontend/e2e/app.e2e-spec.ts
+++ frontend/e2e/app.e2e-spec.ts
@@ -1,4 +1,9 @@
 import { AppPage } from './app.po';
+import { browser, by, element } from 'protractor';
+
+async function getPath() {
+    return new URL(await browser.getCurrentUrl()).pathname;
+}
 
 describe('alkfejl-angular App', () => {
   let page: AppPage;
@@ -7,8 +12,27 @@ describe('alkfejl-angular App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
+  beforeEach(() => {
+        browser.get('/login');
+    });
+    
+  it('should be able to navigate to login', () => {
+        expect(getPath()).toEqual('/login');
+    });
+
+    //rossz login
+    it('should fail to log in for invalid credentials', () => {
+        element(by.css('input[type="username"]')).sendKeys('invaliduser');
+        element(by.css('input[type="password"]')).sendKeys('invalidpassword');
+        element(by.css('input[type="button"]')).click();
+        expect(getPath()).toEqual('/');
+    });
+
+    //jo login
+    it('should log in', () => {
+        element(by.css('input[type="username"]')).sendKeys('bla');
+        element(by.css('input[type="password"]')).sendKeys('blabla');
+        element(by.css('input[type="button"]')).click();
+        expect(getPath()).toEqual('/login');
+    });
 });
